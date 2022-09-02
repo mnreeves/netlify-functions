db6c22e23166addae032edce5a6324cffee89c75
natsir-productions <natsir.productions@gmail.com>
2022-09-03 01:35:30 +0700

    init serverless

---
+++ src/hello.js
@@ -0,0 +1,11 @@
+// src/hello.js
+exports.handler = function(
+  event, // Netlify event
+  context, // Netlify context
+  callback // Your callback
+) {
+  callback(null, {
+    statusCode: 200, // response status code
+    body: 'Hello, World' // response body
+  });
+};
